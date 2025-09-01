dc8cbd1a0ccaabb49c89b0d237a48e14805d0b40
Sharice <drsharice@scannady.net>
2025-08-31 20:27:32 -0400

    chore: update page title(s)

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -1,4 +1,6 @@
+import { useTitle } from "../hooks/useTitle";
 export default function Data() {
+  useTitle("Data | Data Portal");
   return (
     <section className="min-h-screen bg-brand-black text-white pt-24 px-6 grid place-items-center">
       <h1 className="text-5xl md:text-6xl font-extrabold">Data</h1>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { useTitle } from "../hooks/useTitle";
 import Hero from "../components/Hero";
 import ValueProp from "../components/ValueProp";
 import Portfolio from "../components/Portfolio";
 
 export default function Home() {
+  useTitle("Home | Data Portal");
   return (
     <>
       <Hero />
